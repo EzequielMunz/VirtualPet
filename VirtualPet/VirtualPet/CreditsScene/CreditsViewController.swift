1ea92e2d84cc41b058eac762835211a3515df7d7
Nicolas Palmieri <nicolas.palmieri@globant.com>
2014-12-18 18:03:47 -0300

    Creditos hardcodeados

--- VirtualPet/VirtualPet/CreditsScene/CreditsViewController.swift
+++ VirtualPet/VirtualPet/CreditsScene/CreditsViewController.swift
@@ -31,11 +31,29 @@ import UIKit
         person1.setData("Ezequiel Munz", role: "iOS Developer Pro")
         var person2 = CreditPerson()
         person2.setData("NicolasPalmieri", role: "iOS Master")
+        var person3 = CreditPerson()
+        person3.setData("        developTeam>", role: "")
+        var person4 = CreditPerson()
+        person4.setData("", role: "")
+        var person5 = CreditPerson()
+        person5.setData("Max Power", role: "Steve Jobs Teacher")
+        var person6 = CreditPerson()
+        person6.setData("Nicholas Palmieri", role: "iOS Ballskicker")
+        var person7 = CreditPerson()
+        person7.setData("Arnold Scwharzenegger", role: "Terminator")
+        var person8 = CreditPerson()
+        person8.setData("Pechugas Laru", role: "Sucker")
+        var person9 = CreditPerson()
+        person9.setData("Pepita la Pistolera", role: "Gunner")
+        var person10 = CreditPerson()
+        person10.setData("Yoga", role: "El pibe del Cisne")
         
-        creditsData = [person1, person2]
+        creditsData = [person10, person9, person8, person7, person6, person5, person4, person3, person2, person1]
         
         self.creditsTable.reloadData()
         // Do any additional setup after loading the view.
+        
+        self.animationCredits()
     }
     
     override func didReceiveMemoryWarning() {
@@ -61,8 +79,17 @@ import UIKit
         return cell!
     }
     
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Delightful People"
+//    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "developTeam"
+//    }
+    
+    func animationCredits ()
+    {
+        let duration = 2.0
+        let del = 0.0
+        let opt = UIViewAnimationOptions.TransitionFlipFromTop
+        UIView.animateWithDuration (duration, delay: del, options: opt, animations:{
+            self.creditsTable.setContentOffset(CGPointMake(0, 800), animated: false)}, completion: {(bool finished) in})
     }
     
 }
